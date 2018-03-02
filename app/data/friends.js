e747ae7a4e01ee174e951e35d36fece7651d11f3
Beza <bezawit.teferi@gmail.com>
2018-03-02 18:49:07 -0500

    friendsData is linked and available to be used in the apiRoute - Json data is visiable

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,49 @@ var friends = [{
   name: "yoda",
   photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
   scores: [
-"5",
-"1",
-"4",
-"4",
-"5",
-"1",
-"2",
-"5",
-"4",
-"1"
-]
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+    }, {
+      name: "Jake",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+      scores: [
+          "5",
+          "1",
+          "4",
+          "4",
+          "5",
+          "1",
+          "2",
+          "5",
+          "4",
+          "1"
+        ]
 }, {
-  routeName: "darthmaul",
-  name: "Darth Maul",
-  role: "Sith Lord",
-  age: 200,
-  forcePoints: 1200
-}, {
-  routeName: "obiwankenobi",
-  name: "Obi Wan Kenobi",
-  role: "Jedi Master",
-  age: 55,
-  forcePoints: 1350
+  name: "Nati",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+  scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
 }];
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,19 @@
-// all my api get and post happenes here 
+// all my api get and post happenes here
+
+
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendsData = require("../data/friends");
+
 
 module.exports = function(app) {
 
 
 
-
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
 
 
 
