7bf2f7b941eff4c3e64546657500550304a2d14b
Beza <bezawit.teferi@gmail.com>
2018-03-04 23:53:45 -0500

    read me

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Friend-Finder
  Compatibility-based "FriendFinder" application -- basically a dating app. This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+
+### Prerequisites
+ Things you need to install:
+
+1. [express.js](https://www.npmjs.com/package/express)
+2. [bodyParser](https://www.npmjs.com/package/body-parser)
+ 3.  
+ 4.  [path](https://www.npmjs.com/package/path)
+
+
+ ![Friend Finder](friendFinder.gif)
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,16 @@
 var friends = [{
-  name: "Chadwick",
+  name: "Chadwick Boseman",
   photo: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fewedit.files.wordpress.com%2F2015%2F01%2Fchadwick-boseman_612x380_1.jpg%3Fw%3D612&w=800&q=85",
   scores: [
       "5",
+      "3",
+      "2",
+      "1",
+      "5",
+      "3",
+      "1",
+      "2",
+      "4",
       "1",
 
     ]
@@ -10,27 +18,83 @@ var friends = [{
       name: "Micheal B Jordan",
       photo: "http://thatgrapejuice.net/wp-content/uploads/2017/05/michael-b-jordan-tgj-2017-191910.png",
       scores: [
-          "4",
-          "2",
+        "5",
+        "1",
+        "1",
+        "5",
+        "5",
+        "1",
+        "5",
+        "2",
+        "4",
+        "5",
 
         ]
 }, {
-  name: "Lupita",
+  name: "Lupita Nyong'o",
   photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
   scores: [
-      "5",
-      "3",
-
-    ]
-},{
-  name: "Sara",
-  photo: "http://www.hawtcelebs.com/wp-content/uploads/2017/12/lupita-nyong-o-at-star-wars-the-last-jedi-premiere-in-los-angeles-12-09-2017-9.jpg",
-  scores: [
-      "1",
-      "3",
+    "5",
+    "4",
+    "5",
+    "4",
+    "4",
+    "4",
+    "5",
+    "4",
+    "5",
+    "4",
 
     ]
 }
+,{
+  name: "Danai Gurira",
+  photo: "http://celebritycheck.co.zw/wp-content/uploads/2016/01/danai-gurira.jpg",
+  scores: [
+    "5",
+    "5",
+    "3",
+    "3",
+    "1",
+    "5",
+    "4",
+    "3",
+    "3",
+    "3",
+    ]
+},{
+  name: "Daniel Kaluuya",
+  photo: "http://photos.laineygossip.com/thumbs/tmb_173x173_daniel-jordan-globes-08jan18-01.jpg",
+  scores: [
+    "5",
+    "4",
+    "5",
+    "4",
+    "5",
+    "4",
+    "5",
+    "4",
+    "5",
+    "4",
+
+    ]
+},{
+  name: "Andy Serkis",
+  photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTIwNzI2OTA3Nl5BMl5BanBnXkFtZTYwNDIwNzA1._V1_UX214_CR0,0,214,317_AL_.jpg",
+  scores: [
+    "1",
+    "1",
+    "1",
+    "5",
+    "1",
+    "1",
+    "5",
+    "5",
+    "1",
+    "5",
+    ]
+}
+
 
 
 
